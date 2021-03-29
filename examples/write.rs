c3ce14ab6afba2463060f57aacee22693ea0430c
Artur Kovacs <kovacs.artur.barnabas@gmail.com>
2021-03-29 22:47:47 +0200

    Improve example

--- examples/write.rs
+++ examples/write.rs
@@ -8,20 +8,23 @@ use smol_clip::Clipboard;
 
 fn main() -> Result<()> {
     let now = Instant::now();
-
+    let mut i = 0;
     loop {
         for _ in 0..20 {
-            Clipboard::write("yoyoyo")?;
+            i += 1;
+            let text = format!("yoyo-{}", i);
+            println!("\n-- Writing: '{}'", text);
+            Clipboard::write(&text)?;
 
             // thread::sleep(Duration::from_millis(1));
             // println!("\nReading");
-            assert_eq!("yoyoyo", Clipboard::read()?);
+            assert_eq!(text, Clipboard::read()?);
             // println!("\nReading done");
         }
 
         // thread::sleep(Duration::from_millis(100));
 
-        assert_eq!("yoyoyo", Clipboard::read()?);
+        // assert_eq!("yoyoyo", Clipboard::read()?);
         println!("[{}]: success", now.elapsed().as_millis())
     }
 }
